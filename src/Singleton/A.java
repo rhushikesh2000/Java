8911d309dbb4a4c48131d7394219bfcc725a0b45
Rishikesh <Rishikesh@LAPTOP-MRGFOITH>
2023-09-22 10:33:32 +0530

    a

---
+++ src/Singleton/A.java
@@ -0,0 +1,27 @@
+package Singleton;
+
+public class A {
+static	private A a = null;
+
+
+	
+ private A() {
+
+
+	}
+ 
+
+ 
+	public static void main(String[] args) {
+//		A aa = new A();
+//		System.out.println(aa.hashCode());
+//
+//		A bb = new A();
+//		System.out.println(bb.hashCode());
+		System.out.println("rush");
+	}
+
+
+
+
+}
