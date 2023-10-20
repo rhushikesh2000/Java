9b381bf162acab68ffa5caa9b3952cdb7f4280b0
rhushikesh2000 <rhushikeshg71@gmail.com>
2023-10-20 12:50:11 +0530

    adding

---
+++ App.java
@@ -0,0 +1,14 @@
+package com.springmvc.demo1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        car car=new car();
+        car.drive();
+    }
+}
---
+++ car.java
@@ -0,0 +1,11 @@
+package com.springmvc.demo1;
+
+public class car {
+
+	
+	public void drive() {
+		System.out.println("car  is running");
+		
+	}
+	
+}
